3d9b4c5f4cc29544ab19f76c2af20c99fac573ef
31150 <3115089759@qq.com>
2025-07-21 22:07:34 +0800

    更新vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0', // 监听所有 IPv4 地址
+    port: 5555,      // 指定端口（默认 5173）
+    strictPort: true, // 如果端口被占用，直接退出
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:9999',
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
+
+
+// netstat -ano | findstr "5173"
+// TCP    0.0.0.0:5173           0.0.0.0:0              LISTENING       12345
+// taskkill /PID 12345 /F
